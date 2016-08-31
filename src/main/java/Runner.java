cbd4b55b2ac59480d477c2d4c9925eae4d5c9ba7
Kirill <semenovkirill.spb@gmail.com>
2016-09-01 02:28:18 +0300

    introduced spring social api

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import tirnak.persistence.VkAuthorizer;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.springframework.social.vkontakte.api.VKontakteProfile;
+import org.springframework.social.vkontakte.api.impl.VKontakteTemplate;
+import org.springframework.social.vkontakte.api.impl.json.VKArray;
+import tirnak.persistence.VkOAuthorizer;
+
+import java.util.ResourceBundle;
 
 /**
  * Created by kise0116 on 31.08.2016.
@@ -11,14 +18,39 @@ public class Runner {
     public static void main(String[] args) {
                 //Step 1- Driver Instantiation: Instantiate driver object as FirefoxDriver
         WebDriver driver = new FirefoxDriver();
+//
+//        VkAuthorizer authorizer = new VkAuthorizer(driver);
+//        authorizer.authorize();
+//
+//        //Step 4- Close Driver
+//        driver.close();
+//
+//        //Step 5- Quit Driver
+//        driver.quit();
 
-        VkAuthorizer authorizer = new VkAuthorizer(driver);
-        authorizer.authorize();
 
-        //Step 4- Close Driver
-        driver.close();
+        VkOAuthorizer authorizer = new VkOAuthorizer();
+        String toQueryInBrowser = authorizer.getQuery();
 
-        //Step 5- Quit Driver
-        driver.quit();
+        do {
+            driver.navigate().to(toQueryInBrowser);
+            toQueryInBrowser = authorizer.getQuery();
+        } while (!toQueryInBrowser.isEmpty());
+
+        System.out.println(toQueryInBrowser);
+        if (toQueryInBrowser.isEmpty()) {
+            makeFriendsRequest();
+        }
     }
+
+    public static void makeFriendsRequest() {
+        VkOAuthorizer authorizer = new VkOAuthorizer();
+
+        VKontakteTemplate vKontakteTemplate = new VKontakteTemplate(authorizer.getAccessToken(), authorizer.getClientSecret());
+        VKArray<VKontakteProfile> array = vKontakteTemplate.friendsOperations().get();
+        for (VKontakteProfile profile : array.getItems()) {
+            System.out.println(profile.getBirthDate().getDay());
+        }
+    }
+
 }
---
+++ src/main/java/tirnak/persistence/VkOAuthorizer.java
@@ -0,0 +1,61 @@
+package tirnak.persistence;
+
+import org.springframework.social.vkontakte.api.impl.VKontakteTemplate;
+
+import java.util.ResourceBundle;
+
+/**
+ * Created by kirill on 01.09.16.
+ */
+public class VkOAuthorizer {
+    private String clientId;
+    private String clientSecret;
+    private String accessToken;
+    private String code;
+    private String redirect_uri;
+    private String scope;
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    {
+        ResourceBundle myResources = ResourceBundle.getBundle("credentials");
+        clientId = myResources.getString("client_id");
+        clientSecret = myResources.getString("client_secret");
+        accessToken = myResources.getString("access_token");
+        code = myResources.getString("code");
+        redirect_uri = myResources.getString("redirect_uri");
+        scope = myResources.getString("scope");
+    }
+
+    public String getQuery() {
+        if (code.isEmpty() && accessToken.isEmpty()) {
+            return getQueryForCode();
+        } else if (accessToken.isEmpty()) {
+            return getQueryForToken();
+        } else {
+            return "";
+        }
+    }
+
+    private String getQueryForCode() {
+        return "https://oauth.vk.com/authorize?" +
+                "client_id=" + clientId + "&" +
+                "redirect_uri=" + redirect_uri + "&" +
+                "scope=" + scope + "&" +
+                "response_type=code";
+    }
+
+    private String getQueryForToken() {
+        return "https://oauth.vk.com/access_token?" +
+                "client_id=" + clientId + "&" +
+                "client_secret=" + clientSecret + "&" +
+                "redirect_uri=" + redirect_uri + "&" +
+                "code=" + code;
+    }
+}
